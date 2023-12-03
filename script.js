ec61e47e1a33a182ae76dda07d684fe3e11b23c7
rote1400 <rote1400@student.miun.se>
2023-12-03 10:52:36 +0100

    script

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
-console.log("test");
+console.log("Snake game starting");
+
+// Define HTML elements
+const board = document.getElementById('game-dashboard');
+console.log(board);
+
+// Define game variables
+let snake = [{ x: 10, y: 10 }]
+
+// Draw game map, snake, food
+function draw() {
+    board.innerHTML = '';
+    drawSnake();
+}
+
+// Draw snake
+function drawSnake() {
+    snake.forEach((segment) => {
+        const snakeElement = createGameElement('div',
+        'snake');
+
+        setPosition(snakeElement, segment);
+    });
+}
+
+// Create a snake for food div
+function createGameElement(tag, classNameForDiv) {
+    const element = document.createElement(tag);
+    element.className = classNameForDiv;
+
+    return element;
+}
+
+// Set position of snake or food
+function setPosition(element, position) {
+    element.style.gridColumn = position.x;
+    element.style.gridRow = position.y;
+}
